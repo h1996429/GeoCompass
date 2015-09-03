21afca8db69f28c0dfbd38ff803329aa8362db22
HeKa <mofhejia@163.com>
2015-09-03 16:30:37 +0800

    Initial Commit

---
+++ GeoCompass/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  GeoCompass
+//
+//  Created by 何嘉 on 15/9/3.
+//  Copyright (c) 2015年 何嘉. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
