eccf3fddde73dc13637a01ba6115edac93e9110f
HeKa <mofhejia@163.com>
2015-09-03 19:47:53 +0800

    。。。

--- GeoCompass/FirstViewController.swift
+++ GeoCompass/FirstViewController.swift
@@ -9,23 +9,56 @@
 import UIKit
 import CoreMotion
 
-class FirstViewController: UIViewController {
+var strike = 0.00 , dip = 0.00 , dipdir = 0.00 , cefujiao = 0.00 , qingfujiao = 0.00 , qingfuxiang = 0.00;
 
+class FirstViewController: UIViewController {
+    
+    let manager = CMMotionManager();
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
         // Do any additional setup after loading the view, typically from a nib.
-        let manager = CMMotionManager()
+
         if manager.gyroAvailable {
-            manager.deviceMotionUpdateInterval = 0.1
-            let queue = NSOperationQueue()
+            manager.deviceMotionUpdateInterval = 0.1;
+            let queue = NSOperationQueue();
             manager.startDeviceMotionUpdatesToQueue(queue) {
                 [weak self] (data: CMDeviceMotion!, error: NSError!) in
                 // motion processing here
                 NSOperationQueue.mainQueue().addOperationWithBlock {
                     // update UI here
+ 
                 }
             }
 
+        }
+        
+    }
+    
+    func DataCal0(){
+        
+        
+        
+    }
+    func DataCal1(){
+        
+        
+    }
+   
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    @IBAction func indexChange(sender: UISegmentedControl) {
+        switch segmentedControl.selectedSegmentIndex
+        {
+        case 0:
+            self.DataCal0();
+        case 1:
+            self.DataCal1();
+        case 2:
+            manager.stopDeviceMotionUpdates();
+            
+        default:
+            break; 
         }
     }
     
